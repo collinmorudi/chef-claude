1afd9a9aae05116f9e4a30757ad9ee7ad6394927
Collin Morudi <collinmorudi@gmail.com>
2026-07-01 14:27:27 +0200

    feat: functionality to add ingredients

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,59 @@
+import {useState} from "react"
+
 export default function Main() {
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
+    const [ingredients, setIngredients] = useState([])
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
     return (
         <main>
-            <form className="add-ingredient-form">
-                <input 
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
+                <input
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
+
+/*
+
+export default function Main() {
+
+
+    return (
+        <main>
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
+        </main>
+    )
+}
+
+*/
